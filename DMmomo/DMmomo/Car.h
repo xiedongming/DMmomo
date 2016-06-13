c597a91820ad87a36142da6ce4227bc0827c978d
Hello_kugou <18201145070@163.com>
2016-06-13 15:41:25 +0800

    增加car类

---
+++ DMmomo/DMmomo/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  DMmomo
+//
+//  Created by mac_pro on 16/6/13.
+//  Copyright © 2016年 camelot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ DMmomo/DMmomo/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  DMmomo
+//
+//  Created by mac_pro on 16/6/13.
+//  Copyright © 2016年 camelot. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
